a5c143ea8924aee9dae92280f7f596e809967e01
SummerSelness <70924765+SummerSelness@users.noreply.github.com>
2021-03-30 10:36:44 -0700

    Update display_code.py
    added comments and made small adjustments

--- DisplayProject/display_code.py
+++ DisplayProject/display_code.py
@@ -1,27 +1,22 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
-"""
-This test will initialize the display using displayio and draw a solid white
-background, a smaller black rectangle, and some white text.
-"""
 import board
 import displayio
 import terminalio
 from adafruit_display_text import label
 import adafruit_ssd1325
-import time
 
 displayio.release_displays()
 
-# Use for SPI
+# SPI Initialization Code
 spi = board.SPI()
 oled_cs = board.D10
 oled_dc = board.D13
 display_bus = displayio.FourWire(
     spi, command=oled_dc, chip_select=oled_cs, baudrate=1000000, reset=board.D9
 )
-
+# width & height of screen in pixels
 WIDTH = 128
 HEIGHT = 64
 BORDER = 1
@@ -37,15 +32,16 @@ color_bitmap = displayio.Bitmap(display.width, display.height, 1)
 color_palette = displayio.Palette(1)
 color_palette[0] = 0xFFFFFF  # White
 
+# Creates a tile grid at upper left corner of screen
 bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
 splash.append(bg_sprite)
 
 # Draw a first inner rectangle
-inner_bitmap = displayio.Bitmap(41,20, 1)
+inner_bitmap = displayio.Bitmap(41,20, 1) # .Bitmap(width, height, )
 inner_palette = displayio.Palette(1)
 inner_palette[0] = 0x000000  # Black
 inner_sprite = displayio.TileGrid(
-    inner_bitmap, pixel_shader=inner_palette, x=1, y=1)
+    inner_bitmap, pixel_shader=inner_palette, x=1, y=1) # draws at (1,1)
 splash.append(inner_sprite)
 
 # Draw a second inner rectangle
@@ -75,7 +71,7 @@ splash.append(inner_sprite)
 
 text = "Hi     Bye    hello"
 text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
-text_width = text_area.bounding_box[2]
+text_width = text_area.bounding_box[2]*FONTSCALE
 text_group = displayio.Group(
     max_size=10,
     scale=1,
@@ -88,11 +84,11 @@ splash.append(text_group)
 
 text = "42 mph"
 text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF)
-text_width = text_area.bounding_box[2]
+text_width = text_area.bounding_box[2]*FONTSCALE
 text_group = displayio.Group(
     max_size=10,
     scale=3,
-    x=WIDTH // 2 - 105 // 2, #text_width is not correct!
+    x=display.width // 2 - text_width // 2, #text_width is correct!
     y=22 + 42 // 3,
 )
 
